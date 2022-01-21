8bc5c9d2daf00ba25acb8a27f2cfbbe946f2ba83
PaulMouchel <paulmouchel@live.fr>
2022-01-21 20:55:32 +0400

    create sauce works

--- controllers/user.js
+++ controllers/user.js
@@ -11,21 +11,21 @@ exports.signup = (req, res, next) => {
         });
         user.save()
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
+          .catch(error => res.status(400).json({ error }))
       })
-      .catch(error => res.status(500).json({ error }));
-  };
+      .catch(error => res.status(500).json({ error }))
+}
 
-  exports.login = (req, res, next) => {
+exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' })
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+              return res.status(401).json({ error: 'Mot de passe incorrect !' })
             }
             res.status(200).json({
               userId: user._id,
@@ -36,7 +36,7 @@ exports.signup = (req, res, next) => {
               )
             });
           })
-          .catch(error => res.status(500).json({ error }));
+          .catch(error => res.status(500).json({ error }))
       })
-      .catch(error => res.status(500).json({ error }));
-  };
+      .catch(error => res.status(500).json({ error }))
+}
