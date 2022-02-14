6daa1c776ce226c8d1d25b9161175505d3389692
PaulMouchel <paulmouchel@live.fr>
2022-02-14 08:58:29 +0400

    secure with helmet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require("helmet");
 const mongoose = require('mongoose');
 const Sauce = require('./models/sauce');
 require('dotenv').config()
@@ -11,7 +12,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
   
-
+app.use(helmet());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
