9a6741cd481a2717c0941b07689be61ad7047dff
PaulMouchel <paulmouchel@live.fr>
2022-02-21 14:23:04 +0400

    fix issues

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require("helmet");
 const mongoose = require('mongoose');
-const Sauce = require('./models/sauce');
 require('dotenv').config()
 
 const app = express();
@@ -17,6 +16,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy','cross-origin')
     next();
   });
 
